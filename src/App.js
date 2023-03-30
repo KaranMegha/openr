6c6aa254f2a9d8a57cb8ee2a26a3e1beb179459c
KaranMegha <karanmegha99@gmail.com>
2023-03-30 22:34:08 +0530

    Sposnor

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
     <div>
       <MainPage/>
       <Aboutus/>
-      {/* <Sponsor/> */}
+      <Sponsor/>
       <Prize/>
       <Faq/>
     </div>
--- src/components/Sponsor.jsx
+++ src/components/Sponsor.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import './Sponsor.css'
 import heading from '../assests/supported by.png';
-import title from '../assests/title sponsor.png';
+import title from '../assests/bronze sponsor.png';
+import logo from '../assests/NetWeb-Logo-SVG (1).png'
 
 const Sponsor = () => {
   return (
@@ -14,7 +15,7 @@ const Sponsor = () => {
             <img className='sponsorHeading' src={title} alt=""/>
             </div>
             <div className='sponsorImg'>
-
+              <img className='logo' src={logo} alt='' />
             </div>
         </div>
     </div>
